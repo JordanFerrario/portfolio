8b53e2fe5f0528df4ca69f27b92cc1a9ce2696b7
Jordan Ferrario <108853449+JordanFerrario@users.noreply.github.com>
2023-11-15 23:22:52 -0800

    Initial commit

---
+++ assets/script/script.js
@@ -0,0 +1,12 @@
+let navbar = document.getElementById("navbar");
+let sticky = navbar.offsetTop;
+
+function stickyNavbar() {
+    if (window.scrollY >= sticky) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
+};
+
+window.onscroll = function () {stickyNavbar()};
